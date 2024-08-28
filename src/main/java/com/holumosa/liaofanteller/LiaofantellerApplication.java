52b0ca9eb3813f9e931339a53e693b97b894511d
Samson <samson.zheng@tpisoftware.com>
2024-08-29 01:02:02 +0800

    first commit

---
+++ src/main/java/com/holumosa/liaofanteller/LiaofantellerApplication.java
@@ -0,0 +1,13 @@
+package com.holumosa.liaofanteller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiaofantellerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiaofantellerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/holumosa/liaofanteller/LiaofantellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holumosa.liaofanteller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiaofantellerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
